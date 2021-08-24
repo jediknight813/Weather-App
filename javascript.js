735fdbc0c6a9c8ffe15c52b329548e2b20ac3168
-THE-LAST-SAMURAI- <connor813@outlook.com>
2021-08-24 11:34:28 -0500

    updated the ui

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
 
+
+var city_data
+
 let weather_location = "texas"
 
   async function getWeather() {
@@ -8,9 +11,20 @@ let weather_location = "texas"
           mode: 'cors',
         }
       );
-      if (response.status === 404) {
+      if (response.status == 404) {
           console.log("error")
+          return "None"
       }
+      if (response.status == 1006) {
+        console.log("unknown location")
+        return "None"
+      }
+
+      if (response.status == 400) {
+        console.log("unknown location")
+        return "None"
+      }
+
       else {
 
       }
@@ -20,9 +34,37 @@ let weather_location = "texas"
     return data
   }
 
-  getWeather().then(data => console.log(data))
-
-
   
 
-  
+function check_weather_in_city() {
+  weather_location = document.getElementById('search_box_input').value
+  getWeather().then(data => display_data(data))
+}
+
+
+function display_data(data) {
+  console.log(data)
+  city_data = data
+  document.getElementById('city_name_text').innerHTML = (data['location']['country'])
+
+  document.getElementById('temp_name_text').innerHTML = "current temp: " + (data['current']['temp_f']) + "°"
+  document.getElementById('wind_name_text').innerHTML = "wind: " + (data['current']['wind_mph']) + " mph"
+  document.getElementById('condtion_name_text').innerHTML = "condition: " + (data['current']['condition']['text'])
+  document.getElementById('humidity_name_text').innerHTML = "humidity: " + (data['current']['humidity'])
+  document.getElementById('feels_like_name_text').innerHTML = "feels like: " + (data['current']['feelslike_f']) + "°"
+}
+
+function c_f_temp(temp_type) {
+if (temp_type == "c") {
+  document.getElementById('temp_name_text').innerHTML = "current temp: " + (city_data['current']['temp_c']) + "°"
+  document.getElementById('feels_like_name_text').innerHTML = "feels like: " + (city_data['current']['feelslike_c']) + "°"
+}  
+
+if (temp_type == "f") {
+  document.getElementById('temp_name_text').innerHTML = "current temp: " + (city_data['current']['temp_f']) + "°"
+  document.getElementById('feels_like_name_text').innerHTML = "feels like: " + (city_data['current']['feelslike_f']) + "°"
+}  
+
+
+
+}
